8cd8bd4dd4001cdc2f3dc336067dc9c7e1b033e6
Pavel Tarno <paveltarno@gmail.com>
2017-01-27 08:34:13 +0200

    added webpack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, 'frontend')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, function () {
   console.log('jsomatic listening on port 3000!')
